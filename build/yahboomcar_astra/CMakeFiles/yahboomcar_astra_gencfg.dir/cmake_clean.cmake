90bb2de67e41d689cf43915017d558549eac6947
RomainLLC <piactif@gmail.com>
2022-07-23 18:14:56 +0200

    build

---
+++ build/yahboomcar_astra/CMakeFiles/yahboomcar_astra_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yahboomcar_astra_gencfg"
+  "/home/jetson/yahboomcar_ws/devel/include/yahboomcar_astra/KCFTrackerConfig.h"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/KCFTrackerConfig.dox"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/KCFTrackerConfig-usage.dox"
+  "/home/jetson/yahboomcar_ws/devel/lib/python2.7/dist-packages/yahboomcar_astra/cfg/KCFTrackerConfig.py"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/KCFTrackerConfig.wikidoc"
+  "/home/jetson/yahboomcar_ws/devel/include/yahboomcar_astra/ColorTrackerPIDConfig.h"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/ColorTrackerPIDConfig.dox"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/ColorTrackerPIDConfig-usage.dox"
+  "/home/jetson/yahboomcar_ws/devel/lib/python2.7/dist-packages/yahboomcar_astra/cfg/ColorTrackerPIDConfig.py"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/ColorTrackerPIDConfig.wikidoc"
+  "/home/jetson/yahboomcar_ws/devel/include/yahboomcar_astra/ColorHSVConfig.h"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/ColorHSVConfig.dox"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/ColorHSVConfig-usage.dox"
+  "/home/jetson/yahboomcar_ws/devel/lib/python2.7/dist-packages/yahboomcar_astra/cfg/ColorHSVConfig.py"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_astra/docs/ColorHSVConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/yahboomcar_astra_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/yahboomcar_linefollw/CMakeFiles/yahboomcar_linefollw_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yahboomcar_linefollw_gencfg"
+  "/home/jetson/yahboomcar_ws/devel/include/yahboomcar_linefollw/LineDetectPIDConfig.h"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_linefollw/docs/LineDetectPIDConfig.dox"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_linefollw/docs/LineDetectPIDConfig-usage.dox"
+  "/home/jetson/yahboomcar_ws/devel/lib/python2.7/dist-packages/yahboomcar_linefollw/cfg/LineDetectPIDConfig.py"
+  "/home/jetson/yahboomcar_ws/devel/share/yahboomcar_linefollw/docs/LineDetectPIDConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/yahboomcar_linefollw_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
